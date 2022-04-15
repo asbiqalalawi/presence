5951e5016a884c99d96101a3eb0c1cc2266a718c
Asbiq Al Alawi <asbiqalalawi@gmail.com>
2022-04-15 21:05:03 +0700

    Update Password

--- lib/app/modules/update_password/controllers/update_password_controller.dart
+++ lib/app/modules/update_password/controllers/update_password_controller.dart
@@ -1,20 +1,48 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class UpdatePasswordController extends GetxController {
-  //TODO: Implement UpdatePasswordController
+  RxBool isLoading = false.obs;
+  TextEditingController currC = TextEditingController();
+  TextEditingController newC = TextEditingController();
+  TextEditingController confirmC = TextEditingController();
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  void updatePass() async {
+    if (currC.text.isNotEmpty &&
+        newC.text.isNotEmpty &&
+        confirmC.text.isNotEmpty) {
+      if (newC.text == confirmC.text) {
+        isLoading.value = true;
+        try {
+          String email = auth.currentUser!.email!;
+
+          await auth.signInWithEmailAndPassword(
+              email: email, password: currC.text);
+
+          await auth.currentUser!.updatePassword(newC.text);
+
+          Get.back();
+
+          Get.snackbar('Berhasil', 'Password berhasil diubah.');
+        } on FirebaseAuthException catch (e) {
+          if (e.code == 'wrong-password') {
+            Get.snackbar('Terjadi Kesalahan', 'Password salah.');
+          } else {
+            Get.snackbar('Terjadi Kesalahan', e.code.toLowerCase());
+          }
+        } catch (e) {
+          Get.snackbar('Terjadi Kesalahan', 'Tidak dapat mengubah password.');
+        } finally {
+          isLoading.value = false;
+        }
+      } else {
+        Get.snackbar('Terjadi Kesalahan', 'Password baru tidak cocok.');
+      }
+    } else {
+      Get.snackbar('Terjadi Kesalahan', 'Semua input harus diisi.');
+    }
   }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {}
-  void increment() => count.value++;
 }
--- lib/app/modules/update_password/views/update_password_view.dart
+++ lib/app/modules/update_password/views/update_password_view.dart
@@ -5,18 +5,59 @@ import 'package:get/get.dart';
 import '../controllers/update_password_controller.dart';
 
 class UpdatePasswordView extends GetView<UpdatePasswordController> {
+  const UpdatePasswordView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('UpdatePasswordView'),
+        title: const Text('UPDATE PASSWORD'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text(
-          'UpdatePasswordView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: ListView(
+        padding: const EdgeInsets.all(20),
+        children: [
+          TextField(
+            controller: controller.currC,
+            autocorrect: false,
+            obscureText: true,
+            decoration: const InputDecoration(
+                labelText: 'Current Password', border: OutlineInputBorder()),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          TextField(
+            controller: controller.newC,
+            autocorrect: false,
+            obscureText: true,
+            decoration: const InputDecoration(
+                labelText: 'New Password', border: OutlineInputBorder()),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          TextField(
+            controller: controller.confirmC,
+            autocorrect: false,
+            obscureText: true,
+            decoration: const InputDecoration(
+                labelText: 'Confirm New Password',
+                border: OutlineInputBorder()),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Obx(() => ElevatedButton(
+              onPressed: () {
+                if (controller.isLoading.isFalse) {
+                  controller.updatePass();
+                }
+              },
+              child: Text(controller.isLoading.isFalse
+                  ? 'CHANGE PASSWORD'
+                  : 'LOADING...'))),
+        ],
       ),
     );
   }
