a69d553aa0b9b05da6502841c25a9f63f74f87e6
Asbiq Al Alawi <asbiqalalawi@gmail.com>
2022-05-19 20:35:28 +0700

    All Presence Details

--- lib/app/modules/all_presence/controllers/all_presence_controller.dart
+++ lib/app/modules/all_presence/controllers/all_presence_controller.dart
@@ -1,3 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 
-class AllPresenceController extends GetxController {}
+class AllPresenceController extends GetxController {
+  FirebaseAuth auth = FirebaseAuth.instance;
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  Stream<QuerySnapshot<Map<String, dynamic>>> streamAllPresence() async* {
+    String uid = auth.currentUser!.uid;
+
+    yield* firestore
+        .collection('employee')
+        .doc(uid)
+        .collection('presence')
+        .orderBy('date', descending: true)
+        .snapshots();
+  }
+}
--- lib/app/modules/all_presence/views/all_presence_view.dart
+++ lib/app/modules/all_presence/views/all_presence_view.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -35,57 +36,93 @@ class AllPresenceView extends GetView<AllPresenceController> {
             ),
           ),
           Expanded(
-            child: ListView.builder(
-              padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.only(bottom: 20),
-                  child: Material(
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(20),
-                    child: InkWell(
-                      onTap: () => Get..toNamed(Routes.PRESENCE_DETAILS),
-                      borderRadius: BorderRadius.circular(20),
-                      child: Container(
-                        padding: const EdgeInsets.all(20),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  const Text(
-                                    'Masuk',
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.bold),
-                                  ),
-                                  Text(
-                                    DateFormat.yMMMEd().format(DateTime.now()),
-                                    style: const TextStyle(
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                ],
-                              ),
-                              Text(DateFormat.jms().format(DateTime.now())),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              const Text('Keluar',
-                                  style:
-                                      TextStyle(fontWeight: FontWeight.bold)),
-                              Text(DateFormat.jms().format(DateTime.now())),
-                            ]),
+            child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+                stream: controller.streamAllPresence(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  if (snapshot.data!.docs.isEmpty ||
+                      snapshot.data?.docs == null) {
+                    return const SizedBox(
+                      height: 150,
+                      child: Center(
+                        child: Text('Belum ada history presensi.'),
                       ),
-                    ),
-                  ),
-                );
-              },
-            ),
+                    );
+                  }
+                  return ListView.builder(
+                    padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+                    itemCount: snapshot.data!.docs.length,
+                    itemBuilder: (context, index) {
+                      Map<String, dynamic> data =
+                          snapshot.data!.docs[index].data();
+
+                      return Padding(
+                        padding: const EdgeInsets.only(bottom: 20),
+                        child: Material(
+                          color: Colors.grey[200],
+                          borderRadius: BorderRadius.circular(20),
+                          child: InkWell(
+                            onTap: () => Get.toNamed(Routes.PRESENCE_DETAILS,
+                                arguments: data),
+                            borderRadius: BorderRadius.circular(20),
+                            child: Container(
+                              padding: const EdgeInsets.all(20),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        const Text(
+                                          'Masuk',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        Text(
+                                          DateFormat.yMMMEd().format(
+                                            DateTime.parse(
+                                              data['date'],
+                                            ),
+                                          ),
+                                          style: const TextStyle(
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                      ],
+                                    ),
+                                    Text(
+                                      DateFormat.jms().format(
+                                        DateTime.parse(data['masuk']?['date']),
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    const Text('Keluar',
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold)),
+                                    Text(data['keluar']?['date'] != null
+                                        ? DateFormat.jms().format(
+                                            DateTime.parse(
+                                              data['keluar']['date'],
+                                            ),
+                                          )
+                                        : '-'),
+                                  ]),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                }),
           ),
         ],
       ),
